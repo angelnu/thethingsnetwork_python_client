15e7182cb8ca7bf7c088c91651693d14752f1bff
Angel Nunez Mencias <git@angelnu.com>
2024-08-21 18:41:22 +0200

    black formating fix

--- src/ttn_client/const.py
+++ src/ttn_client/const.py
@@ -5,7 +5,7 @@ from typing import Final
 from aiohttp import ClientTimeout
 
 
-DEFAULT_TIMEOUT : Final[ClientTimeout] = ClientTimeout(total=10 * 60)
+DEFAULT_TIMEOUT: Final[ClientTimeout] = ClientTimeout(total=10 * 60)
 TTN_DATA_STORAGE_URL = (
     "https://{hostname}/api/v3/as/applications/"
     "{app_id}/packages/storage/uplink_message{options}"
